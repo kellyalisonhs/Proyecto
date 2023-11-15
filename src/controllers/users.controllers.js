64dcfdab018744ff727aa46b6a06ff674e2ee9c9
magaliggonzalezz <95144102+magaliggonzalezz@users.noreply.github.com>
2023-11-14 22:33:48 -0600

    views

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -2,17 +2,39 @@ import * as userService from '../services/users.service.js';
 
 export const getAllUsers = (req, res)=>{
    // consumo de la promesa retornada en el servicio que accede a la BD  
-   
    console.log(userService.getAllUsers());
    /* Consumo con async y await */
    (async () => {      
       let users = await userService.getAllUsers(); 
-      res.render("users",{"title":"Lista de usuarios registrados",
-                           "list":users});      
+      res.render("users",{"list":users});      
     })();      
 };
 
-export const login = (req, res)=>{
+export const getRegisterForm = (req, res) => {
+   res.render("register.hbs")
+}
+
+export const getLoginForm = (req, res) => {
+   res.render("login.hbs")
+}
+
+export const getUsers = (req, res) => {
+   res.render("usersList.hbs")
+}
+
+export const getForgotPasswd = (req, res) => {
+   res.render("forgotPasswd.hbs")
+}
+
+export const getPasswdRecovery = (req, res) => {
+   res.render("passwdRecovery.hbs")
+}
+
+export const getChangePasswd = (req, res) => {
+   res.render("changePasswd.hbs")
+}
+
+/* export const login = (req, res)=>{
    let {email, passwd} = req.body;
    
    console.log(email+' '+passwd);
@@ -23,4 +45,4 @@ export const login = (req, res)=>{
          res.send("Usuario registrado");
        })();            
    }
-}
+} */
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { getAllUsers, login } from "../controllers/users.controllers.js"
+import { getChangePasswd, getForgotPasswd, getLoginForm, getPasswdRecovery, getRegisterForm, getUsers } from "../controllers/users.controllers.js"
 
 const router = Router();
 
-router.get("/users", getAllUsers);
-router.post("/login", login);
+//router.get("/users", getAllUsers);
+//router.post("/login", login);
+
+router.get("/change-passwd", getChangePasswd);
+router.get("/forgot-passwd", getForgotPasswd);
+router.get("/login", getLoginForm);
+router.get("/recovery-passwd", getPasswdRecovery);
+router.get("/register", getRegisterForm);
+router.get("/users-list", getUsers);
 
 export default router;
